57e6af93abb13c8a04f861f6e32205a1dc20cca8
Shubhadarshie Nanda <nandamikushubha@gmail.com>
2020-07-10 15:44:58 +0530

    modifi

--- src/component/main/main.js
+++ src/component/main/main.js
@@ -1,38 +1,62 @@
 import React, { Component } from 'react';
 import './main.css'
 import { Button } from '@material-ui/core';
-import { Redirect } from 'react-router-dom';
+import Axios from "axios";
+import Spinner from "../Spinner/Spinner";
+import Cards from "../cards/cards";
 
 class Main extends Component{
     constructor(){
         super();
         this.state = {
-            redirect: false
+            page_url: "",
+            loading: false,
+            message: null,
+            willAppear: null
         }
     }
 
-    onSubmit = () => {
-        this.setState({redirect: true});
+    inputChange = (e) => {
+        this.setState({page_url: e.target.value});
+    }
+
+    handleSubmit = async (e) => {
+        this.setState({ message: null, willAppear: null });
+        e.preventDefault();
+        const pageUrl = this.state.page_url;
+        const url = "http://localhost:8080/addScrapper";
+        try{
+            this.setState({loading: true});
+            const result = await Axios.post(url,{pageUrl: pageUrl});
+            console.log(result);
+            this.setState({
+                message: result.data.message,
+                willAppear: result.data.name
+            });
+        }catch(err){
+            console.log(err);
+        }
+        this.setState({loading: false})
     }
 
     render(){
-        let redirect = null;
-        if(this.state.redirect){
-            redirect = <Redirect to="/table" />
-        }
         return(
+            <div>
                 <div>
-                    <div>
-                        <h2>Enter websites to monitor</h2>
-                        <input className="input" placeholder="Add website"></input>
-                        <div className="btn"><Button variant="contained" color="primary">Add</Button></div>
-                    </div>
-                    <div className="added-sites">
-                        <h3>Websites under monitor</h3>   
-                        <Button variant="contained" color="primary" onClick={this.onSubmit}>StartupTalky</Button>
-                    </div>
-                    {redirect}
+                    <h2>Enter websites to monitor</h2>
+                    <form onSubmit={this.handleSubmit}>
+                        <input id="input" className="input" placeholder="Add website" onChange={this.inputChange}></input>
+                        <div className="btn"><Button variant="contained" type="submit" color="primary">Add Website</Button></div>
+                    </form>
                 </div>
+                {this.state.message ? (
+                    <div style={{ color: "black" }}>{this.state.message}</div>
+                  ) : (
+                    ""
+                  )}
+                {this.state.loading ? <Spinner /> : null}
+                {this.state.willAppear ? <Cards /> : null}
+            </div>
         );
     }
 }
